a32bf2d68b91a144d222b4b961fd235db61823cc
Jade Mattos <jademfcoelho@gmail.com>
2023-03-10 09:00:12 -0300

    add arquivos

---
+++ script.js
@@ -0,0 +1,255 @@
+let aviso = document.querySelector("#aviso");
+let pontos = 0; // pontos para o placar
+let placar = 0; // placar
+
+// PERGUNTA
+let pergunta = document.querySelector("#pergunta");
+
+// ALTERNATIVAS
+let a = document.querySelector("#a");
+let b = document.querySelector("#b");
+let c = document.querySelector("#c");
+let d = document.querySelector("#d");
+
+// article com a class questoes
+let articleQuestoes = document.querySelector(".questoes");
+// ol li com as alternativas
+let alternativas = document.querySelector("#alternativas");
+
+const q0 = {
+  numQuestao: 0,
+  pergunta: "Pergunta",
+  alternativaA: "Alternativa A",
+  alternativaB: "Alternativa B",
+  alternativaC: "Alternativa C",
+  alternativaD: "Alternativa D",
+  correta: "0",
+};
+const q1 = {
+  numQuestao: 1,
+  pergunta: "Boreal é o mesmo que...",
+  alternativaA: "Sul",
+  alternativaB: "Leste",
+  alternativaC: "Norte",
+  alternativaD: "Oeste",
+  correta: "Norte",
+};
+const q2 = {
+  numQuestao: 2,
+  pergunta: "Qual é a capital do Brasil?",
+  alternativaA: "Rio de Janeiro",
+  alternativaB: "Brasília",
+  alternativaC: "Salvador",
+  alternativaD: "Lisboa",
+  correta: "Brasília",
+};
+const q3 = {
+  numQuestao: 3,
+  pergunta: "Austral é o mesmo que...",
+  alternativaA: "Oeste",
+  alternativaB: "Leste",
+  alternativaC: "Norte",
+  alternativaD: "Sul",
+  correta: "Sul",
+};
+const q4 = {
+  numQuestao: 4,
+  pergunta: "A linha do Equador divide a Terra em...",
+  alternativaA: "Leste e Oeste",
+  alternativaB: "Norte e Sul",
+  alternativaC: "Verão e Inverno",
+  alternativaD: "Solstícios e Eclipses",
+  correta: "Norte e Sul",
+};
+const q5 = {
+  numQuestao: 5,
+  pergunta: "Nascente é o mesmo que...",
+  alternativaA: "Lado que o sol nasce",
+  alternativaB: "Abaixo do Equador",
+  alternativaC: "Lado que o sol se põe",
+  alternativaD: "Acima do Equador",
+  correta: "Lado que o sol nasce",
+};
+const q6 = {
+  numQuestao: 6,
+  pergunta: "Qual é o clima predominante do Brasil?",
+  alternativaA: "Polar",
+  alternativaB: "Tropical",
+  alternativaC: "Desértico",
+  alternativaD: "Temperado do Norte",
+  correta: "Tropical",
+};
+const q7 = {
+  numQuestao: 7,
+  pergunta: "Poente é o mesmo que...",
+  alternativaA: "Lado onde o sol se põe",
+  alternativaB: "Lado onde o sol nasce",
+  alternativaC: "Abaixo do Equador",
+  alternativaD: "Acima do Equador",
+  correta: "Lado onde o sol se põe",
+};
+const q8 = {
+  numQuestao: 8,
+  pergunta: "O Brasil se localiza em qual continente?",
+  alternativaA: "África",
+  alternativaB: "Europa",
+  alternativaC: "Oceania",
+  alternativaD: "América",
+  correta: "América",
+};
+const q9 = {
+  numQuestao: 9,
+  pergunta: "Qual é a única capital do Brasil cortada pela linha do Equador?",
+  alternativaA: "Belém",
+  alternativaB: "São Luís",
+  alternativaC: "Macapá",
+  alternativaD: "Boa Vista",
+  correta: "Macapá",
+};
+const q10 = {
+  numQuestao: 10,
+  pergunta: "Considerando a extensão territorial o Brasil é o ...",
+  alternativaA: "3º maior",
+  alternativaB: "2º maior",
+  alternativaC: "4º maior",
+  alternativaD: "5º maior",
+  correta: "5º maior",
+};
+
+// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
+const questoes = [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
+
+let numero = document.querySelector("#numero");
+let total = document.querySelector("#total");
+
+numero.textContent = q1.numQuestao;
+
+let totalDeQuestoes = questoes.length - 1;
+console.log("Total de questões " + totalDeQuestoes);
+total.textContent = totalDeQuestoes;
+
+// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
+pergunta.textContent = q1.pergunta;
+a.textContent = q1.alternativaA;
+b.textContent = q1.alternativaB;
+c.textContent = q1.alternativaC;
+d.textContent = q1.alternativaD;
+
+// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
+a.setAttribute("value", "1A");
+b.setAttribute("value", "1B");
+c.setAttribute("value", "1C");
+d.setAttribute("value", "1D");
+
+// PARA MONTAR AS PROXIMAS QUESTOES
+function proximaQuestao(nQuestao) {
+  numero.textContent = nQuestao;
+  pergunta.textContent = questoes[nQuestao].pergunta;
+  a.textContent = questoes[nQuestao].alternativaA;
+  b.textContent = questoes[nQuestao].alternativaB;
+  c.textContent = questoes[nQuestao].alternativaC;
+  d.textContent = questoes[nQuestao].alternativaD;
+  a.setAttribute("value", nQuestao + "A");
+  b.setAttribute("value", nQuestao + "B");
+  c.setAttribute("value", nQuestao + "C");
+  d.setAttribute("value", nQuestao + "D");
+}
+
+// VERIFICAR DUPLO CLICK NAS ALTERNATIVAS
+alternativas.addEventListener("dblclick", () => {
+  //console.log('Duplo clique')
+  // pontos -= 10; // tirar 10 pontos em caso de duplo click
+  // pontos = pontos
+  if (numQuestao.value == 10 && pontos == 110) {
+    pontos = 100;
+  }
+});
+
+function bloquearAlternativas() {
+  alternativas.classList.add("bloqueado");
+}
+
+function desbloquearAlternativas() {
+  alternativas.classList.remove("bloqueado");
+}
+
+function piscarNoAcerto(r) {
+  r.classList.remove("errou");
+  r.classList.add("acertou");
+}
+
+function piscarNoErro(r) {
+  r.classList.remove("acertou");
+  r.classList.add("errou");
+}
+
+function tirarPiscar(r) {
+  r.classList.remove("acertou");
+  r.classList.remove("errou");
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
+  let numeroDaQuestao = nQuestao.value;
+  console.log("Questão " + numeroDaQuestao);
+
+  let respostaEscolhida = resposta.textContent;
+  //console.log("RespU " + respostaEscolhida)
+
+  let certa = questoes[numeroDaQuestao].correta;
+  //console.log("RespC " + certa)
+
+  if (respostaEscolhida == certa) {
+    piscarNoAcerto(resposta);
+    pontos += 10; // pontos = pontos + 10
+    if (nQuestao.value == 1 && pontos == 20) {
+      pontos = 10;
+    }
+  } else {
+    piscarNoErro(resposta);
+  }
+  setTimeout(() => {
+    tirarPiscar(resposta);
+  }, 150);
+
+  // atualizar placar
+  placar = pontos;
+  instrucoes.textContent = "Pontos: " + placar;
+
+  // bloquear a escolha de opcoes
+  bloquearAlternativas();
+
+  setTimeout(function () {
+    let proxima = numeroDaQuestao + 1;
+
+    if (proxima > totalDeQuestoes) {
+      console.log("Fim do Jogo!");
+      fimDoJogo();
+    } else {
+      proximaQuestao(proxima);
+    }
+  }, 150);
+  desbloquearAlternativas();
+}
+
+let btn = document.querySelector("button");
+function fimDoJogo() {
+  let s;
+  pontos == 0 ? (s = "") : (s = "s");
+  instrucoes.textContent = `Fim de jogo! Você conseguiu ${pontos} ponto${s}`;
+
+  instrucoes.classList.add("placar");
+
+  // OCULTAR O ARTICLE DA QUESTAO
+  articleQuestoes.style.display = "none";
+  btn.style.display = "block";
+  aviso.style.display = "none";
+
+  btn.addEventListener("click", () => {
+    pontos = 0; // zerar placar
+    location.reload();
+    instrucoes.classList.remove("placar");
+    // REINICIAR O JOGO
+    articleQuestoes.style.display = "block";
+    proximaQuestao(1);
+  });
+}
